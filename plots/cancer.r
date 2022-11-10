75e01a085686f5604348000484ddab4241fc94ee
CarsonFoster <carsonwfoster@gmail.com>
2022-11-10 10:01:56 -0600

    Add cancer plot

---
+++ plots/cancer.r
@@ -0,0 +1,18 @@
+cancer <- query("
+SELECT Question, avg(Data_value) as avg_data, Break_Out FROM brfss
+WHERE Question REGEXP 'cancer|depression|tobacco|heart' AND Sample_Size > 50
+      AND Data_value IS NOT NULL AND Break_Out_Category = 'Age Group'
+      AND Response = 'Yes'
+GROUP BY Break_Out, Question;
+")
+cancer$Break_Out <- as.factor(cancer$Break_Out)
+
+ggplot(cancer, aes(group = Question)) +
+    geom_line(aes(x = Break_Out, y = avg_data, color = Question, size = I(2))) +
+    labs(title = "Conditions by Age Group",
+         x = "Age Group", y = "Percent of Age Group") +
+    theme(
+        legend.position = 'bottom',
+        legend.direction = 'vertical'
+    )
+
