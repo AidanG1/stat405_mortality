0246b841662db4004dc3918656acecae6948d778
CarsonFoster <carsonwfoster@gmail.com>
2022-11-22 11:17:19 -0600

    Change layout, add palette selection

--- shiny.r
+++ shiny.r
@@ -1,25 +1,65 @@
 library(shiny)
 
+coloredBox <- function(color) {
+    div(style = paste0("background-color:", color, "; width: 20px; height: 20px"))
+}
+
+palette <- function(colors) {
+    do.call(div, c(style = "display: flex; flex-direction: row", lapply(colors, coloredBox)))
+}
+
+palettes <- list(
+    default = person_colors,
+    option1 = c("goldenrod", "dimgrey", "blue", "maroon", "lightblue", "red")
+)
+
+choices <- unname(do.call(tagList, lapply(palettes, palette)))
+values <- names(palettes)
+
 ui <- fluidPage(
-    titlePanel("Mortality Killer"),
-    sidebarLayout(
-        sidebarPanel(
+    verticalLayout(
+        titlePanel("Mortality Killer"),
+        plotOutput("killerPlot", height = "500px"),
+        wellPanel(
             sliderInput(
                 "scale",
                 label = "Scale all:",
                 min = 0.1, value = 1, max = 3
+            ),
+            radioButtons(
+                "palette",
+                "Color Palette",
+                choiceNames = choices,
+                choiceValues = values,
+                inline = TRUE
             )
-        ),
-        mainPanel(
-            plotOutput("killerPlot")
         )
     )
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         sliderInput(
+    #             "scale",
+    #             label = "Scale all:",
+    #             min = 0.1, value = 1, max = 3
+    #         ),
+    #         radioButtons(
+    #             "palette",
+    #             "Color Palette",
+    #             choiceNames = choices,
+    #             choiceValues = values,
+    #             inline = TRUE
+    #         )
+    #     ),
+    #     mainPanel(
+    #         plotOutput("killerPlot")
+    #     )
+    # )
 )
 
 server <- function(input, output) {
     output$killerPlot <- renderPlot({
         print("Killer Plot Server Start")
-        draw_killer(person_colors, input$scale)
+        draw_killer(palettes[input$palette][[1]], input$scale)
         print("Killer Plot Server Done")
     })
 }
